56d2cf6cd12a24193b88df10e8da4a81d9a51831
vijayedysor <67878124+vijayedysor@users.noreply.github.com>
2021-10-04 14:46:37 +0530

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import WebviewScreen from '../components/WebviewScreen';
+const Home = () => {
+  return <View>
+    <Text>Home</Text>
+  </View>
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
